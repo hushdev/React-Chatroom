77272babb59e92bb25dae0f12f8c9ef1e35383fa
hushdev <ildar.hushdev@gmail.com>
2020-09-28 00:08:36 +0500

    Updates

---
+++ precache-manifest.6c925a40e31d76499f1db956b7e33eac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26476e4ed7be4cd8d283fd6dc843096f",
+    "url": "/react-chat/index.html"
+  },
+  {
+    "revision": "ba613ec5020cbb4de2fc",
+    "url": "/react-chat/static/css/main.4ff44d42.chunk.css"
+  },
+  {
+    "revision": "b6c24ab104f246d13dd6",
+    "url": "/react-chat/static/js/2.15943995.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/react-chat/static/js/2.15943995.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba613ec5020cbb4de2fc",
+    "url": "/react-chat/static/js/main.df3cf148.chunk.js"
+  },
+  {
+    "revision": "0b78552f60f8f83f3b34",
+    "url": "/react-chat/static/js/runtime-main.0c1104e4.js"
+  },
+  {
+    "revision": "6850c1de452ce6aecb5595b94a166d28",
+    "url": "/react-chat/static/media/send.6850c1de.svg"
+  }
+]);
